c8c1683d8e702e3bceacc81af9f32be7890cff81
Your Name <you@example.com>
2023-09-03 09:36:18 +0530

    this is the tools update

---
+++ tools/infog.bat
@@ -0,0 +1,36 @@
+start firefox www.twitter.com
+start firefox www.youtube.com
+start firefox www.facebook.com
+start firefox www.quora.com
+start firefox www.instagram.com
+start firefox https://www.google.com/search?q=latest+hacking+news
+start firefox https://www.google.com/search?q=latest+finance+news
+start firefox https://www.google.com/search?q=news
+C:\Users\abdul\AppData\Local\Discord\app-1.0.9016\Discord.exe
+start firefox https://www.infosecurity-magazine.com/
+start firefox www.github.com
+start firefox www.gitlab.com
+start firefox www.thehackernews.com
+start firefox https://www.cybersecurity-insiders.com/
+start firefox https://www.lastwatchdog.com/
+start firefox https://cyware.com/cyber-security-news-articles
+start firefox www.linkedin.com
+start firefox https://www.hackerone.com/press
+start firefox https://www.bugcrowd.com/blog/
+start firefox https://blog.intigriti.com
+start firefox https://www.synack.com/blog/
+start firefox https://portswigger.net/blog
+start firefox www.gmail.com
+start firefox www.binance.com/en/feed
+start firefox www.polygon.technology/blog
+start firefox www.reddit.com
+"C:\Users\abdul\AppData\Roaming\Telegram Desktop\Telegram.exe"
+
+
+
+
+
+
+
+
+
